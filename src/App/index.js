63a0bc36a5745ed408da92be434218177de0557b
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-28 21:36:45 -0300

    fetch data with useEffect

--- src/App/index.js
+++ src/App/index.js
@@ -1,3 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export const App = () => <div>Hello</div>
+export const App = () => {
+	const [users, setUsers] = useState([])
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch('https://api.github.com/users')
+			const data = await response.json()
+			console.log(data)
+			setUsers(data)
+		}
+		fetchData()
+	}, [])
+	if (users.length)
+		return <div>{users[0].login}</div>
+	return <div>No users</div>
+}
